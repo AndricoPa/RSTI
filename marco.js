7d020a1b90b8cb4c33997d23fbb509112e97b70d
AndricoPa <andricomico@gmail.com>
2024-08-15 16:49:11 -0300

    arquivo novo

---
+++ marco.js
@@ -0,0 +1,11 @@
+const amorzinhos = ["Luciana Exequiel", "Bruna Beacker", "Pedro Exequiel Ribeiro", "Leandro Whats", "Marco Antonio", "Xandy","Piratini","Lais"]
+
+class Amor{
+    constructor(array){
+        this.array = array;
+    }
+
+    amores(array){
+        return array;
+    }
+}
